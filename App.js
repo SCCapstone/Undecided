daeebd253dca7a7ad06b5bea9a2be30bb5896191
Caleb Corpening <70110218+corpeningc@users.noreply.github.com>
2022-10-17 17:56:39 -0400

    change default text

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={style.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>KYS</Text>
       <StatusBar style="auto" />
     </View>
   );

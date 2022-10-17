969ba1abebc59bc4b1ad57d3383f22d31d09b39a
Caleb Corpening <70110218+corpeningc@users.noreply.github.com>
2022-10-17 17:55:19 -0400

    change styles to style

--- App.js
+++ App.js
@@ -3,14 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={style.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',

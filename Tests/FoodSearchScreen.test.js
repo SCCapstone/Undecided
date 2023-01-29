a1c26b1de2f7d48947569bb1cb597b9fc8d99d2d
Caleb Corpening <70110218+corpeningc@users.noreply.github.com>
2023-01-29 16:33:28 -0500

    first unit - test

--- Tests/FoodSearchScreen.test.js
+++ Tests/FoodSearchScreen.test.js
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import FoodSearchScreen from '../Screens/FoodSearchScreen';
-import {render, screen, fireEvent} from '@testing-library/react-native'
-
+import {render, screen, fireEvent,} from '@testing-library/react-native';
 describe('FoodSearchScreen', () => {
+  const thisRoute = { params: { meal: 'breakfast' }};
+  beforeEach(() => {
+    var wrapper = render(<FoodSearchScreen route={thisRoute}/>);
+  });
   it('ham search returns results', async () => {
-    render(FoodSearchScreen({route: {params: {meal: 'Breakfast'}}}))
     const searchInput = screen.getByPlaceholderText('Search');
     fireEvent.changeText(searchInput, 'ham');
-    searchInput.onEndEditing();
-    const results = await screen.findAllByTestId('searchResult');
-    console.log(results);
+    fireEvent(searchInput, 'onEndEditing');
+    const results = screen.getAllByTestId('foodList');
+    expect(results).not.toBeNull();
   });
 });

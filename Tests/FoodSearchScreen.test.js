f3d47a2090c4c7dbab5eeb11ad9071e494bbebb8
Caleb Corpening <70110218+corpeningc@users.noreply.github.com>
2023-01-29 13:50:31 -0500

    test update

--- Tests/FoodSearchScreen.test.js
+++ Tests/FoodSearchScreen.test.js
@@ -1,3 +1,14 @@
 import * as React from 'react';
 import FoodSearchScreen from '../Screens/FoodSearchScreen';
-import render from '@testing-library/react-native';
+import {render, screen, fireEvent} from '@testing-library/react-native'
+
+describe('FoodSearchScreen', () => {
+  it('ham search returns results', async () => {
+    render(FoodSearchScreen({route: {params: {meal: 'Breakfast'}}}))
+    const searchInput = screen.getByPlaceholderText('Search');
+    fireEvent.changeText(searchInput, 'ham');
+    searchInput.onEndEditing();
+    const results = await screen.findAllByTestId('searchResult');
+    console.log(results);
+  });
+});

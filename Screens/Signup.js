e80ed7cec1a7605dcd59b4d876f2f2ca20ae90ca
Mostafa Mohamed Ali <70235876+MostafaMohAli@users.noreply.github.com>
2023-02-17 16:37:57 -0500

    updated activities

--- Screens/Signup.js
+++ Screens/Signup.js
@@ -29,11 +29,10 @@ export default function Signup({ navigation: { navigate } }) {
   });
 
   const activities = [
-    { _id: "1", value: "1" },
-    { _id: "2", value: "2" },
-    { _id: "3", value: "3" },
-    { _id: "4", value: "4" },
-    { _id: "5", value: "5" },
+    { _id: "1", value: "Sedentary" },
+    { _id: "2", value: "Light" },
+    { _id: "3", value: "Moderate" },
+    { _id: "4", value: "High" },
   ];
 
   const goals = [

e483d1d935d69b846b4e694d7d7d404f714cb578
peytontucker <peytonmtucker@gmail.com>
2023-04-01 21:22:37 -0400

    added code to actually allow change of email and password

--- Screens/Settings/AccountSettings.js
+++ Screens/Settings/AccountSettings.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { getAuth, updateEmail, updatePassword, reauthenticateWithCredential, EmailAuthProvider, signInWithEmailAndPassword, onAuthStateChanged} from "firebase/auth"
 import { View, FlatList, TouchableWithoutFeedback, Keyboard, Button } from "react-native";
 import { globalStyles } from "../../styles/global";
 import Setting from "../../components/Setting";
@@ -18,10 +19,16 @@ export default function AccountSettings({ navigation }) {
     ]);
 
     const [userDocSnap, setUserDocSnap] = useState(null);
+    const [auth, setAuth] = useState(getAuth())
 
     useEffect(() => {
         getUserDocSnap();
     }, []);
+
+    //necessary because user may update same setting during same session multiple times
+    useEffect(() => {
+        getUserDocSnap();
+    }, [settings]);
 
     const getUserDocSnap = async () => {
         const uid = await AsyncStorage.getItem("uid");
@@ -33,13 +40,66 @@ export default function AccountSettings({ navigation }) {
     const saveChangesHandler = async () => {
         const uid = await AsyncStorage.getItem("uid");
         const userDocRef = doc(db, "users", uid);
+
+
         settings.forEach(async element => {
             if (element.data != null) {
                 await updateDoc(userDocRef, {
                     [element.dbField]: element.data
                 });
+
+                //set data to null to allow future capture of changes
+                element.data = null;
             }
         });
+
+        //handle email and password sequentially
+        if (settings[2].data != null && settings[3].data != null) {
+            await handleEmailChange(settings[2].data).then(settings[2].data = null);
+            await handlePasswordChange(settings[3].data).then(settings[3].data = null);
+        } else if (settings[2].data != null) {
+            await handleEmailChange(settings[2].data).then(settings[2].data = null);
+        } else if (settings[3].data != null) {
+            await handlePasswordChange(settings[3].data).then(settings[3].data = null);
+        }
+    }
+
+    const handleEmailChange = async (newEmail) => {
+        //refresh userDocSnap to ensure up-to-date info
+        const email = userDocSnap.get(dbConstants.EMAIL)
+        console.log(email)
+        const password = userDocSnap.get(dbConstants.PASSWORD)
+
+        const credential = EmailAuthProvider.credential(email, password)
+        console.log("Obtained credential: " + credential)
+
+        signInWithEmailAndPassword(auth, email, password).then( (user) => {
+            console.log("signed in successfully")
+            reauthenticateWithCredential(auth.currentUser, credential).then(() => {
+            //update email
+            console.log("attempting email update to " + newEmail)
+            updateEmail(auth.currentUser, newEmail).catch((error) => {
+                console.log("Email update failed after reauthentication attempt.")
+            })}
+        )})
+    }
+
+    const handlePasswordChange = async (newPassword) => {
+        const email = userDocSnap.get(dbConstants.EMAIL)
+        const password = userDocSnap.get(dbConstants.PASSWORD)
+
+            const credential = EmailAuthProvider.credential(email, password)
+            console.log("Obtained credential: " + credential)
+
+            signInWithEmailAndPassword(auth, email, password).then( (user) => {
+                console.log("signed in successfully")
+                reauthenticateWithCredential(auth.currentUser, credential).then(() => {
+                //update email
+                console.log("attempting password update to " + newPassword)
+                updatePassword(auth.currentUser, newPassword).catch((error) => {
+                    console.log("Password update failed after reauthentication attempt.")
+                })}
+            )})
     }
 
     const handleCallback = (item, newData) => {
